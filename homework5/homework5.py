3256a7a32218887330f4d0c21d1925c23d08cf85
IodinP <168574718+IodinP@users.noreply.github.com>
2025-02-17 18:21:49 +0500

    fix

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -4,7 +4,10 @@ while True:
     user_input = input("Введите число или 'stop'/'end' для завершения: ")
     
     if user_input.lower() in ['stop', 'end']:
-        
         break
-    sum = sum+int(user_input)    
+    elif user_input.isdigit():  # Проверяем, является ли ввод числом
+        sum += int(user_input)  # Если число, добавляем к сумме
+    else:
+        print("Пожалуйста, введите число или 'stop'/'end'.")  # Если не число и не команда
+    
 print(f"Сумма введенных чисел: {sum}")
